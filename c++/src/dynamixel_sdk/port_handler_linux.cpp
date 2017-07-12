b52cf3eb83c820ec482689eee087bdb682533401
leon <rwjung@robotis.com>
2017-07-12 14:02:50 +0900

    Mac OS supports DynamixelSDK

--- c++/src/dynamixel_sdk/port_handler_linux.cpp
+++ c++/src/dynamixel_sdk/port_handler_linux.cpp
@@ -242,30 +242,30 @@ int PortHandlerLinux::getCFlagBaud(int baudrate)
       return B115200;
     case 230400:
       return B230400;
-    // case 460800:
-    //   return B460800;
-    // case 500000:
-    //   return B500000;
-    // case 576000:
-    //   return B576000;
-    // case 921600:
-    //   return B921600;
-    // case 1000000:
-    //   return B1000000;
-    // case 1152000:
-    //   return B1152000;
-    // case 1500000:
-    //   return B1500000;
-    // case 2000000:
-    //   return B2000000;
-    // case 2500000:
-    //   return B2500000;
-    // case 3000000:
-    //   return B3000000;
-    // case 3500000:
-    //   return B3500000;
-    // case 4000000:
-    //   return B4000000;
+    case 460800:
+      return B460800;
+    case 500000:
+      return B500000;
+    case 576000:
+      return B576000;
+    case 921600:
+      return B921600;
+    case 1000000:
+      return B1000000;
+    case 1152000:
+      return B1152000;
+    case 1500000:
+      return B1500000;
+    case 2000000:
+      return B2000000;
+    case 2500000:
+      return B2500000;
+    case 3000000:
+      return B3000000;
+    case 3500000:
+      return B3500000;
+    case 4000000:
+      return B4000000;
     default:
       return -1;
   }
