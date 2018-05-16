c55b759fb412d48102793f0a0d0191fc6c5e65ac
leon <rwjung@robotis.com>
2018-05-16 13:45:25 +0900

    deprecated functions in packethandler.h remained was removed

--- c++/include/dynamixel_sdk/packet_handler.h
+++ c++/include/dynamixel_sdk/packet_handler.h
@@ -101,25 +101,11 @@ class WINDECLSPEC PacketHandler
   virtual const char *getTxRxResult     (int result) = 0;
 
   ////////////////////////////////////////////////////////////////////////////////
-  /// @brief The function that prints out description of communication result
-  /// @param result Communication result which might be gotten by the tx rx functions
-  /// @todo This function is deprecated (removed in DynamixelSDK ver. 3.6.1)
-  ////////////////////////////////////////////////////////////////////////////////
-  DEPRECATED virtual void printTxRxResult          (int result) = 0;
-
-  ////////////////////////////////////////////////////////////////////////////////
   /// @brief The function that gets description of hardware error
   /// @param error Dynamixel hardware error which might be gotten by the tx rx functions
   /// @return description of hardware error in const char* (string)
   ////////////////////////////////////////////////////////////////////////////////
   virtual const char *getRxPacketError  (uint8_t error) = 0;
-
-  ////////////////////////////////////////////////////////////////////////////////
-  /// @brief The function that prints out description of hardware error
-  /// @param error Dynamixel hardware error which might be gotten by the tx rx functions
-  /// @todo This function is deprecated (removed in DynamixelSDK ver. 3.6.1)
-  ////////////////////////////////////////////////////////////////////////////////
-  DEPRECATED virtual void printRxPacketError       (uint8_t error) = 0;
 
   ////////////////////////////////////////////////////////////////////////////////
   /// @brief The function that transmits the instruction packet txpacket via PortHandler port.
