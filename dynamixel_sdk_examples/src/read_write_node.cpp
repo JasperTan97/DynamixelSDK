2d092e32f8cec58a66859546321e7adc0dee548d
Will Son <willson@robotis.com>
2021-02-23 16:21:50 +0900

    abide lint

--- dynamixel_sdk_examples/src/read_write_node.cpp
+++ dynamixel_sdk_examples/src/read_write_node.cpp
@@ -1,18 +1,16 @@
-/*******************************************************************************
-* Copyright 2020 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+// Copyright 2021 ROBOTIS CO., LTD.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 /*******************************************************************************
  * This example is written for DYNAMIXEL X(excluding XL-320) and MX(2.0) series with U2D2.
@@ -117,7 +115,7 @@ ReadWriteNode::ReadWriteNode()
         portHandler,
         (uint8_t) request->id,
         ADDR_PRESENT_POSITION,
-        (uint32_t *)&present_position,
+        reinterpret_cast<uint32_t *>&present_position,
         &dxl_error
       );
 
